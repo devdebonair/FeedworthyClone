633a75aafdab771f9f5a2884009964f3df48846a
Vincent Moore <devdebonair@gmail.com>
2016-05-09 12:08:57 -0400

    Initial Commit

---
+++ Feedworthy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Feedworthy
+//
+//  Created by Vincent Moore on 5/9/16.
+//  Copyright © 2016 Vincent Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
