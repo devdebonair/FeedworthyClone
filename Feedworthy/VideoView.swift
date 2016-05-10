446c89ffdcb1df80f320ed3ed53f97a630149c75
Vincent Moore <devdebonair@gmail.com>
2016-05-10 08:01:40 -0400

    commented video view

--- Feedworthy/VideoView.swift
+++ Feedworthy/VideoView.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2016 Vincent Moore. All rights reserved.
 //
 
+/*
+    Needed to create a custom view for the video player.
+    This allowed the video layer to resize based on constraints
+    during the layoutSubviews function call.
+ */
+
+
 import UIKit
 import AVFoundation
 
@@ -21,17 +28,22 @@ class VideoView: UIView {
     
     init(frame: CGRect = CGRectZero, url: String? = nil) {
         _url = url
+        
         if let url = _url, loadedUrl = NSURL(string: url) {
             _player = AVPlayer(URL: loadedUrl)
         } else {
             _player = AVPlayer()
         }
+        
         _playerLayer = AVPlayerLayer(player: _player)
         _playerLayer.videoGravity = AVLayerVideoGravityResizeAspect
+        
         super.init(frame: frame)
+        
         layer.addSublayer(_playerLayer)
     }
     
+    // Not implemented for xibs
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
